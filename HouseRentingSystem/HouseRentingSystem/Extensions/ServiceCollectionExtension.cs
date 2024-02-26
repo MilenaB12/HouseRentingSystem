a906dfba5cbf70ca56c1a838466304276cc3b0f8
Milena Boyadzhieva <123003398+MilenaB12@users.noreply.github.com>
2024-02-26 16:04:40 +0200

    Added ServiceCollectionExtension and clered Program.cs

---
+++ HouseRentingSystem/HouseRentingSystem/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using HouseRentingSystem.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Extensions
+{
+	public static class ServiceCollectionExtension
+	{
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddAplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+
+        public static IServiceCollection AddAplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
+
