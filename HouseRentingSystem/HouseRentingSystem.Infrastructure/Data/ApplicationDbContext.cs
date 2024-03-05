52cfb276520e8b1ee57e2406068b5a7f715118f4
Milena Boyadzhieva <123003398+MilenaB12@users.noreply.github.com>
2024-03-05 16:53:35 +0200

    add migration

--- HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/ApplicationDbContext.cs
+++ HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -9,6 +9,24 @@ public class ApplicationDbContext : IdentityDbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<House>()
+            .HasOne(h => h.Category)
+            .WithMany(c => c.Houses)
+            .HasForeignKey(h => h.CategoryId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Entity<House>()
+        .HasOne(h => h.Agent)
+        .WithMany(c => c.Houses)
+        .HasForeignKey(h => h.AgentId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+        base.OnModelCreating(builder);
     }
 
     public DbSet<Agent> Agents { get; set; } = null!;
